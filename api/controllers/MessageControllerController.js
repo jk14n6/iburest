c689c7ab47363c20126f7c0e6c646527f9e1f403
jon_khan <klang.jonathan@gmail.com>
2015-03-14 15:40:09 -0400

    responding to sms from user

---
+++ api/controllers/MessageControllerController.js
@@ -0,0 +1,118 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+
+module.exports = {
+
+    /*
+     *
+     * EMAIL
+     *
+     */
+	invite: function(req, res) {
+
+        var date = req.param('date');
+        var body = req.param('text') || '';
+        var hostId = req.user; // celui qui invite
+        var invitedId = req.param('invited');
+
+        // send sms invite
+        SmsService.invite(hostId, invitedId, body, date, function(err, message) {
+    		if(!err) {
+	            // logging
+	            console.log('Success! The SID for this SMS message is:');
+	            console.log(message.sid);
+	            console.log('Message sent on:');
+	            console.log(message.dateCreated);
+
+	            //EmailService.invite(hostId, invitedId, date);
+                res.send(200);
+           //          res.send(200);
+	          // //   	if(!err) {
+	          // //   		res.send(200);
+	          // //   	}
+	        		// // else {
+	        		// // 	res.send(400);
+        			// // }
+	          //   });
+
+	        }
+	        else {
+                res.send(400);
+            }
+        });
+    },
+
+    sendEmail: function(req, res) {
+    	var to = req.param('to');
+		var message = req.param('message');
+
+    	EmailService.send(to, message);
+    },
+
+    sendtextemail: function(req, res) {
+    	var to = req.param('to');
+    	var fromAddress = req.param('fromaddress');
+    	var fromName = req.param('fromname');
+    	var subject = req.param('subject');
+    	var message = req.param('message');
+
+    	EmailService.sendtext(fromAddress, fromName, to, subject, message, function (err, message) {
+    		if(!err) {
+    			console.log('message sent successfully!');
+    			res.send(200);
+    		}
+    		else
+    			res.send(400);
+    	});
+    },
+
+    /*
+     * SMS 
+     */
+    // simple sms sending
+    sendSms: function(req, res) {
+    	var to = req.param('to');
+    	var body = req.param('body');
+
+    	SmsService.send(to, body, function(err, message) {
+    		if(!err) {
+    			//logging
+	            console.log('Success! The SID for this SMS message is:');
+	            console.log(message.sid);
+	            console.log('Message sent on:');
+	            console.log(message.dateCreated);
+
+	            res.send(200);
+    		}
+    		else
+    			res.send(400);
+    	})
+    }
+
+    /*
+     * This rest service will only be called by twilio when a user
+     * wants to confirm it's phone number by replying to the sms
+     * the application has sent him. 
+     * Please do not alter.
+     */
+    twiliocbconfirmnumber : function(req, res) {
+        var messageSid 	= req.param('MessageSid');
+        var accountSid 	= req.param('AccountSid');
+        var from 		= req.param('From');
+        var to 			= req.param('To');
+        var body 		= req.param('Body');
+        var fromCity 	= req.param('FromCity');
+        var fromZip 	= req.param('FromZip');
+        var fromCountry	= req.param('FromCountry');
+
+        //if(body.toUpperCase() === 'YES')
+        	console.log('User with phone number [' + from + '] has confirmed it\'s number via sms'.);
+        	EmailService.send('klang.jonathan@gmail.com', body);
+    }
+};
+
---
+++ api/services/EmailService.js
@@ -0,0 +1,117 @@
+var api_key = 'key-7b54fbef79a56c2ec89e2a2bc241da1c';
+//var api_key = process.env.MAILGUNAPIKEY;
+var domain = 'mail.ibu.io';
+var mailgunjs = require('mailgun-js')({apiKey: api_key, domain: domain});
+// var mailcomposer = new MailComposer();
+
+
+// ibu mailing lists
+var ibuApplicantsList = mailgunjs.lists('applicants@mail.ibu.io');	// should be @gogoexotic.com
+var ibuClubsList      = mailgunjs.lists('clubs@mail.ibu.io');			// ``
+
+// GogoExotic mailing lists
+var gogoApplicantsList = mailgunjs.lists('applicants@mail.gogoexotic.com');	
+var gogoClubsList      = mailgunjs.lists('clubs@mail.gogoexotic.com');
+
+// iBu different senders
+var fromIbuSupportName = 'iBu Suuport Team';
+var fromIbuSupportAddr = 'support@ibu.io';
+var fromIbuNoReplyName = 'iBu Team';
+var fromIbuNoReplyAddr = 'noreply@ibu.io';
+
+// GogoExotic different senders
+var fromGogoSupportName = 'gogOeXotic Support Team';
+var fromGogoSupportAddr = 'support@gogoexotic.com';
+var fromGogoNoReplyName = 'gogOeXotic Team';
+var fromGogoNoReplyAddr = 'noreply@gogoexotic.com';
+
+var Mailgun = require('mailgun').Mailgun;
+var mailgun = new Mailgun(api_key);
+
+module.exports = {
+
+	sendhtml: function (from, to, subject, html, cb) {
+		var message = 'From: ' + from;
+		message += '\nTo: ' + to;
+		message += '\nContent-Type: text/html; charset=utf-8';
+		message += '\nSubject: ' + subject;
+		message += '\n\n' + html;
+
+		mailgun.sendRaw(from, to, message, cb);
+	},
+
+	sendtext: function(fromAddr, fromName, to, subject, text, cb) {
+		var data = {
+  			from: fromName + ' <' + fromAddr + '>',
+  			to: to,
+  			subject: subject,
+  			text: text
+		};
+ 
+		mailgunjs.messages().send(data, cb);
+	},
+
+	sendActivation: function (user) {
+		if (typeof user.profile.contactEmail === 'undefined') {
+			return false;
+		}
+		var html = '<a href=\'https://gogoexotic-core.herokuapp.com/gateway/activate?activation_key=' + user.activationKey + '\'>click here</a> to activate your account';
+		var message = 'From: admin@ibu.io';
+		message += '\nTo: ' + user.profile.contactEmail;
+		message += '\nContent-Type: text/html; charset=utf-8';
+		message += '\nSubject: GogoExotic Account Verification';
+		message += '\n\n' + html;		
+
+		mailgun.sendRaw('admin@ibu.io', user.profile.contactEmail, message, function () {});
+	},
+
+	// send invitation 
+	sendinvite: function(hostId, invitedId, inviteDate) {
+		User.findOne(hostId).then(function(fromUser) {
+			User.findOne(invitedId).then(function(toUser) {
+				if(typeof toUser.profile.email === 'undefined') {
+					return false;
+				}
+				var htmlMessage = '<p> Hi, ' + fromUser.username + ' has invited you to work on ' + inviteDate + 
+                            '. To answer contact at: ' + fromUser.profile.phone + ' or ' + fromUser.email + '. </p>';
+
+                var textMessage = 'Hi, ' + fromUser.username + ' has invited you to work on ' + inviteDate + 
+                            '. To answer contact at: ' + fromUser.profile.phone + ' or ' + fromUser.email + '.';
+
+        		var data = {
+        			from: fromGogoNoReplyName + fromGogoNoReplyAddr,
+        			to: toUser.profile.email,
+        			subject: 'Gogo New Invitation!',
+        			text: textMessage
+        		};
+
+        		mailgun.messages().send(data, function (err, body) {
+              // cb(err, message);
+        		});
+			})
+		})
+	},
+
+	sendhtmltolist: function(from, to, subject, html, cb) {
+
+	},
+
+	sendtexttolist: function(from, to, subject, html, cb) {
+
+	},
+
+	addusertolist: function(user, list, cb) {
+
+	},
+
+	removeuserfromlist: function(user, list, cb) {
+
+	},
+}
+
+
+
+
+
+
+
