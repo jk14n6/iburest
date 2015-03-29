54205fc5ed187f8f736f41d183500fc67a6b04c7
jon_khan <klang.jonathan@gmail.com>
2015-03-29 15:25:00 -0400

    stripe

--- api/controllers/StripeCbController.js
+++ api/controllers/StripeCbController.js
@@ -4,12 +4,26 @@
  * @description :: Server-side logic for managing Stripecbs
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var request = require('request');
 
 module.exports = {
 	connectUserCb : function(req, res) {
-		console.log(req.param('response_type'));
-		console.log(req.param('client_id'));
+		console.log(req.param('code'));
 		console.log(req.param('scope'));
+
+		request.post('https://connect.stripe.com/oauth/token',
+		    { form: { 
+		    		client_secret: process.env.STRIPE_KEY_PRIVATE,
+		    		code : req.param('code'),
+		    		grant_type : authorization_code
+	    		} 
+    		},
+		    function (error, response, body) {
+		        if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
+		            console.log(body)
+		        }
+		    }
+		);
 
 		res.send('OK');
 	}
