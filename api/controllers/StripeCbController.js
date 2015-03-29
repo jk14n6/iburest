6771fc510600a7b60eff3f5d20dbe184751df262
jon_khan <klang.jonathan@gmail.com>
2015-03-29 14:48:43 -0400

    stripe

---
+++ api/controllers/StripeCbController.js
@@ -0,0 +1,13 @@
+/**
+ * StripeCbController
+ *
+ * @description :: Server-side logic for managing Stripecbs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	connectUserCb : function(req, res) {
+		console.log(req);
+	}
+};
+
