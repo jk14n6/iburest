7104023b3cd9648cee39303b09f20ff20d0498a0
jon_khan <klang.jonathan@gmail.com>
2015-04-06 15:37:45 -0400

    stripe

--- api/controllers/StripeCbController.js
+++ api/controllers/StripeCbController.js
@@ -16,13 +16,14 @@ module.exports = {
 	 * see: https://stripe.com/docs/connect for more info
 	 */
 	accountAuthorize : function(req, res) {
-		log(req.param('code'));
-		log(req.param('scope'));
 
 		var authCode = req.param('code');					// used to make a POST request to Stripe access_token_url endpoint
 		var scope = req.param('scope');						// dictates what iBu platform will be able to do on behalf of the connected account
 		var stripeErr = req.param('error');					// Stripe error name, if any
 		var StripeErrDesc = req.param('error_description'); // stripe error description
+
+		log('code: '  + authCode);
+		log('scope: ' + scope);
 
 		// get the user id
 		//var user = ...
@@ -49,30 +50,22 @@ module.exports = {
 		*/
 		request.post('https://connect.stripe.com/oauth/token',
 		    { form: { 
-		    		client_secret: process.env.STRIPE_CLIENT_ID,
+		    		client_id: process.env.STRIPE_CLIENT_ID,
 		    		code : authCode,
-		    		grant_type : 'authorization_code'
+		    		grant_type : 'authorization_code',
+		    		client_secret : process.env.STRIPE_KEY_PRIVATE
 	    		} 
     		},
 		    function (error, response, body) {
 		        if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
 		            log(JSON.stringify(body, null, 2));
 
-		            //{
-		            //  "token_type": "bearer",
-		            //  "stripe_publishable_key": PUBLISHABLE_KEY,
-		            //  "scope": "read_write",
-		            //  "livemode": false,
-		            //  "stripe_user_id": USER_ID,
-		            //  "refresh_token": REFRESH_TOKEN,
-		            //  "access_token": ACCESS_TOKEN
-		            //}
 		            StripeAccount.create({token_type:body.token_type}).exec(function createCB(err, created) {
 		            	log('Created Stripe Account: ' + JSON.stringify(created, null, 2));
 		            });
 		        }
 		        else if(error) {
-		        	log()
+		        	log(JSON.stringify(error, null, 2));
 		        }
 		    }
 		);
